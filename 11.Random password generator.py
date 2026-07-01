8c8e8305c9e72b426d2a3695622d610695ed87a9
HARI SARAN <harisaran479@gmail.com>
2026-07-01 23:50:24 +0530

    Add random password generator functionality

---
+++ 11.Random password generator.py
@@ -0,0 +1,28 @@
+#random password generator
+import random, string
+
+#step1 : define password geenrator function
+def generate_password(length=12):
+  if length<4:
+    raise ValueError("Password length must be atleast 4 characters")
+  uppercase=string.ascii_uppercase
+  lowercase=string.ascii_lowercase
+  digits=string.digits
+  special_chars="!@#$%^&*_+-=[]{}:;<>?/"
+  password=[
+      random.choice(uppercase),
+      random.choice(lowercase),
+      random.choice(digits),
+      random.choice(special_chars)
+  ]
+  all_chars=uppercase+lowercase+digits+special_chars
+  password += random.choices(all_chars,k=length - 4)
+  random.shuffle(password)
+  return ''.join(password)
+
+try:
+  length=int(input("Enter the desired password length(minimum 4): "))
+  password=generate_password(length)
+  print("Generated Password:",password)
+except ValueError as e:
+  print(e)
